c297cb617e79843d77d5bd94202591e5b3cf56cc
merrylmr <736633757@qq.com>
2024-01-24 12:48:34 +0800

    初始化列表页面

--- demo/js/neonCursor.js
+++ demo/js/neonCursor.js
@@ -1,4 +1,4 @@
-import {neonCursor} from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+import {neonCursor} from 'threejs-toys'
 
 neonCursor({
     el: document.getElementById('app'),
